f8f56982e6522b416b9640d2c38a70a4b69e3bee
Tobi Oyero <tobioyero@gmail.com>
2022-04-30 14:38:14 -0400

    Changed grid to use display: grid from flexbox

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 //Choose DOM nodes
-const grid = document.querySelector('#grid-container'); //grid for boxes
+const grid = document.querySelector('#grid_container'); //grid for boxes
 const clearButton = document.querySelector('.clear-grid'); //clear button
 const squareDial = document.querySelector('#changeSquares'); // changes number of squares
 let dimensionCheck = document.querySelector('.square_set');// to be able to see the dimensions of the grid
 let colorPicker = document.querySelector('#favColor'); //color picker 
     let currColor = 'blue'; //sets default color to blue
-let squaresPerSide = squareDial.value;
+let squaresPerSide = 16;
 let cells = [];
 
 //function to create the grid
 function createGrid(squaresPerSide){
-    grid.innerHTML = '';
-    let numOfCells = squaresPerSide ** 2;
-    let cellSize = grid.clientWidth / squaresPerSide;
+    grid.style.gridTemplateColumns = `repeat(${squaresPerSide}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${squaresPerSide}, 1fr)`;
+    let cells = [];
     
-    for(let i = 0; i < numOfCells; i++){
-        cells[i] = document.createElement('div');
-        cells[i].classList.add('cell');
-        cells[i].style.width = cellSize + 'px';
-        cells[i].style.height = cellSize + 'px';
-        grid.appendChild(cells[i]);
+    for (let i = 0; i < squaresPerSide ** 2; i++){
+      cells[i] = document.createElement('div');
+      cells[i].classList.add('cell');
+      
+      grid.appendChild(cells[i]);
+      }
     }
-}
+    
 
 
 
