9623b89bb28a405753b330f1575c1aec2b3adb81
Tobi Oyero <tobioyero@gmail.com>
2022-05-02 16:13:11 -0400

    Added rainbow color randomizer

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const clearButton = document.querySelector('.clear-grid'); //clear button
 const squareDial = document.querySelector('#changeSquares'); // changes number of squares
 let dimensionCheck = document.querySelector('.square_set');// to be able to see the dimensions of the grid
 let colorPicker = document.querySelector('#favColor'); //color picker 
+let rainbowButton = document.querySelector('#rainbow')
     let currColor = 'blue'; //sets default color to blue
+    let rainbow = false;
 let squaresPerSide = 16;
 let cells = [];
 
@@ -42,18 +44,35 @@ function changeSquares(e){
     dimensionCheck.textContent = `${squaresPerSide} x ${squaresPerSide}`;
 }
 
+// function to make a rainbow 'pen' 
+function rainbow_button(){
+//if rainbow button is pressed then on each movement from cell to cell then the color should change
+// -> figure out how to randomize the color 
+// -> make it so it changes on every square
+
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    currColor = "#" + randomColor;
+    rainbow = true;
+ }
+
 
 //Adding Event Listeners to nodes
 clearButton.addEventListener('click', clearGrid);
 squareDial.addEventListener('input', changeSquares);
 squareDial.addEventListener('mouseup', clearGrid);
 grid.addEventListener('mouseover', function (e) {
+    if (rainbow){
+        rainbow_button();
+    }
     if (e.target.matches('.cell')){
         e.target.style.backgroundColor = `${currColor}`;
     }
 });
 
+rainbowButton.addEventListener('click', rainbow_button);
+
 colorPicker.addEventListener('input', function(e){
     currColor = e.target.value;
+    rainbow = false;
 })
 window.addEventListener('onload', createGrid(squaresPerSide))
